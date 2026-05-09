489ec336b09fba4a42c3cf51dfb704f5501df553
yukkuri <reityan.sakura0104@gmail.com>
2026-05-09 18:35:57 +0900

    B4 fix

--- src/20260509/B4/script.js
+++ src/20260509/B4/script.js
@@ -7,15 +7,15 @@ colorinp.addEventListener("input", () => {
     color = colorinp.value;
     colorout.style.backgroundColor = color
     
-    colorR = color.slice(1,3)
-    colorG = color.slice(3,5)
-    colorB = color.slice(5,7)
-    
-    //16->10進数に変換...
-    R = String(colorR).toString(10)
-    G = String(colorG).toString(10)
-    B = String(colorB).toString(10)
+    let colorR = color.slice(1,3)
+    let colorG = color.slice(3,5)
+    let colorB = color.slice(5,7)
+     
+    let R = parseInt(colorR,16)
+    let G = parseInt(colorG,16)
+    let B = parseInt(colorB,16)
     console.log(R);
+    
     
 
     RGB = `rgb(${R},${G},${B})`
