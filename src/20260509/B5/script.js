d40b6ac266bfd4977ba63278587b6772e75b95ef
yukkuri <reityan.sakura0104@gmail.com>
2026-05-09 18:50:10 +0900

    B5 fix

--- src/20260509/B5/script.js
+++ src/20260509/B5/script.js
@@ -1,5 +1,19 @@
 const button = document.querySelector("button")
 
-document.addEventListener("scroll", (e) => {
-    button.style.display = "block"
+window.addEventListener("scroll", () => {
+    const scroll = window.scrollY
+    const view = window.innerHeight
+
+    console.log(scroll, view);
+
+    if (scroll + view === 5000) {
+        button.style.display = "block"
+        button.addEventListener("click", () => {
+            button.style.display = "none"
+            window.scrollTo(0,0)
+        })
+    } else {
+        button.style.display = "none"
+    }
+
 })
